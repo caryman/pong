f56183ba240cba864feb99882d5a7c1e09c319e8
caryman <tom@tsfactory.com>
2021-09-17 06:42:28 -0400

    added colors, playfield objects, size, specs

--- src/Pong.hs
+++ src/Pong.hs
@@ -12,6 +12,8 @@ data Point = Point { _x :: Int
                    , _y :: Int
                    } deriving (Eq, Show)
 
+type Position = Point
+ 
 data Velocity = Velocity { _dx :: Int
                          , _dy :: Int
                          } deriving (Eq, Show)
@@ -28,11 +30,51 @@ data Pong = Pong { _ball :: Ball
                  , _paddles :: (Paddle, Paddle)
                  } deriving (Eq, Show)
 
+data ColorRGB = RGB { _red :: Int
+                    , _blue :: Int
+                    , _green :: Int } deriving (Eq, Show)
+
+data ColorHSV = HSV { _hue :: Int
+                    , _saturation :: Int
+                    , _value :: Int } deriving (Eq, Show)
+
+data ColorHex = Int 
+
+data Color = ColorRGB Int Int Int | ColorHSV Int Int Int | ColorHex Int deriving (Eq, Show)
+
+data Colors = Colors { _background :: Color
+                     , _foreground :: Color } deriving (Eq, Show)
+
+data PlayFieldSize = PlayFieldSize { _width :: Int
+                                   , _height :: Int
+                                   , _resolution :: Int } deriving (Eq, Show)
+
+data PlayFieldSpecs = PlayFieldSpecs { _size :: PlayFieldSize
+                                     , _color :: Colors } deriving (Eq, Show)
+
+data PlayFieldObjects = PlayfieldObjects { _object :: PlayFieldObject
+                                         , _position :: Position
+                                         , _velocity :: Velocity
+                                         , _visible :: Bool
+                                         , _spin :: Int } deriving (Eq, Show)
+
+data PlayFieldObject = PlayFieldObject { _name :: String
+                                       , _width :: Int
+                                       , _height :: Int
+                                       , _color :: Color
+                                       , _bumpcolor :: Color } deriving (Eq, Show)
+
 makeFieldsNoPrefix ''Point
 makeFieldsNoPrefix ''Velocity
 makeFieldsNoPrefix ''Ball
 makeFieldsNoPrefix ''Paddle
 makeFieldsNoPrefix ''Pong
+makeFieldsNoPrefix ''ColorRGB
+makeFieldsNoPrefix ''ColorHSV
+makeFieldsNoPrefix ''PlayFieldSize
+makeFieldsNoPrefix ''PlayFieldSpecs
+makeFieldsNoPrefix ''PlayFieldObjects
+makeFieldsNoPrefix ''PlayFieldObject
 
 
 
