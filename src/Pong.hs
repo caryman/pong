9caf36109b3e9d9804f74d728f7fc437539a64ff
Jeremy Maness <jwmaness@gmail.com>
2021-10-09 20:50:01 -0400

    Introduced type synonym for Pong monad transformer stack

--- src/Pong.hs
+++ src/Pong.hs
@@ -6,62 +6,80 @@
 
 module Pong where
 
-import Control.Lens
+import           Control.Lens
+import           Control.Monad.State
+import qualified UI.NCurses as N
 
---type Game = StateT Pong Curses ()
+type Pong a = StateT PongState N.Curses a
 
-data Ball = Ball { _position :: (Integer, Integer)
-                 , _velocity :: (Integer, Integer)
-                 } deriving (Eq, Show)
+data Ball = Ball
+  { _position :: (Integer, Integer)
+  , _velocity :: (Integer, Integer)
+  } deriving (Eq, Show)
 
-data Paddle = Paddle { _position :: (Integer, Integer)
-                     , _height :: Integer
-                     } deriving (Eq, Show)
+data Paddle = Paddle
+  { _position :: (Integer, Integer)
+  , _height :: Integer
+  } deriving (Eq, Show)
 
-data Pong = Pong { _ball :: Ball
-                 , _paddles :: (Paddle, Paddle)
-                 , _playFieldSpecs :: PlayFieldSpecs
-                 , _score :: (Integer, Integer)
-                 , _speed :: Int
-                 } deriving (Eq, Show)
+data PongState = PongState
+  { _ball :: Ball
+  , _paddles :: (Paddle, Paddle)
+  , _playFieldSpecs :: PlayFieldSpecs
+  , _score :: (Integer, Integer)
+  , _speed :: Int
+  } deriving (Eq, Show)
 
-data ColorRGB = RGB { _red :: Integer
-                    , _blue :: Integer
-                    , _green :: Integer } deriving (Eq, Show)
+data ColorRGB = RGB
+  { _red :: Int
+  , _blue :: Int
+  , _green :: Int
+  } deriving (Eq, Show)
 
-data ColorHSV = HSV { _hue :: Integer
-                    , _saturation :: Integer
-                    , _value :: Integer } deriving (Eq, Show)
+data ColorHSV = HSV
+  { _hue :: Int
+  , _saturation :: Int
+  , _value :: Int
+  } deriving (Eq, Show)
 
-data ColorHex = Integer 
+data ColorHex = Int
 
-data Color = ColorRGB Integer Integer Integer | ColorHSV Integer Integer Integer | ColorHex Integer deriving (Eq, Show)
+data Color = ColorRGB Int Int Int | ColorHSV Int Int Int | ColorHex Int deriving (Eq, Show)
 
-data Colors = Colors { _background :: Color
-                     , _foreground :: Color } deriving (Eq, Show)
+data Colors = Colors
+  { _background :: Color
+  , _foreground :: Color
+  } deriving (Eq, Show)
 
-data PlayFieldSize = PlayFieldSize { _width :: Integer
-                                   , _height :: Integer
-                                   , _resolution :: Integer } deriving (Eq, Show)
+data PlayFieldSize = PlayFieldSize
+  { _width :: Integer
+  , _height :: Integer
+  , _resolution :: Integer
+  } deriving (Eq, Show)
 
-data PlayFieldSpecs = PlayFieldSpecs { _size :: PlayFieldSize
-                                     , _color :: Colors } deriving (Eq, Show)
+data PlayFieldSpecs = PlayFieldSpecs
+  { _size :: PlayFieldSize
+  , _color :: Colors
+  } deriving (Eq, Show)
 
-data PlayFieldObjects = PlayfieldObjects { _object :: PlayFieldObject
-                                         , _position :: (Integer, Integer)
-                                         , _velocity :: (Integer, Integer)
-                                         , _visible :: Bool
-                                         , _spin :: Integer } deriving (Eq, Show)
+data PlayFieldObjects = PlayfieldObjects
+  { _object :: PlayFieldObject
+  , _position :: (Integer, Integer)
+  , _velocity :: (Integer, Integer)
+  , _visible :: Bool
+  , _spin :: Integer } deriving (Eq, Show)
 
-data PlayFieldObject = PlayFieldObject { _name :: String
-                                       , _width :: Integer
-                                       , _height :: Integer
-                                       , _color :: Color
-                                       , _bumpcolor :: Color } deriving (Eq, Show)
+data PlayFieldObject = PlayFieldObject
+  { _name :: String
+  , _width :: Integer
+  , _height :: Integer
+  , _color :: Color
+  , _bumpcolor :: Color
+  } deriving (Eq, Show)
 
 makeFieldsNoPrefix ''Ball
 makeFieldsNoPrefix ''Paddle
-makeFieldsNoPrefix ''Pong
+makeFieldsNoPrefix ''PongState
 makeFieldsNoPrefix ''ColorRGB
 makeFieldsNoPrefix ''ColorHSV
 makeFieldsNoPrefix ''PlayFieldSize
@@ -75,10 +93,10 @@ _x = _1
 _y :: Field2 s t a b => Lens s t a b
 _y = _2
 
-leftPaddle :: Lens' Pong Paddle
+leftPaddle :: Lens' PongState Paddle
 leftPaddle = paddles . _1
 
-rightPaddle :: Lens' Pong Paddle
+rightPaddle :: Lens' PongState Paddle
 rightPaddle = paddles . _2
 
 data KeyAction = NoAction | Quit | LPaddleUp | LPaddleDn | RPaddleUp | RPaddleDn | Restart | Pause | Faster | Slower deriving (Eq, Show) 
